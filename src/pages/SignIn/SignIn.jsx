4a183fbb92934cb6a3eb92b1aa09592d8e5cb3ce
LireEruel <jsh001505@gmail.com>
2023-04-07 18:11:59 +0900

    Assignment 1, 2 충족

--- src/pages/SignIn/SignIn.jsx
+++ src/pages/SignIn/SignIn.jsx
@@ -1,10 +1,11 @@
 import { requestSignIn } from "api/api";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import swal from "sweetalert";
 
 function SignIn() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [disabled, setDisabled] = useState(true);
   const trySignIn = async () => {
     const signupResult = await requestSignIn(email, password);
     if (signupResult.succses) {
@@ -17,16 +18,38 @@ function SignIn() {
       text: signupResult.message,
     });
   };
+
+  useEffect(() => {
+    if (email.search("@") > -1 && password.length > 7) {
+      setDisabled(false);
+    } else {
+      setDisabled(true);
+    }
+  }, [email, password]);
   return (
     <div>
       <h1>SignIn</h1>
-      <label>email</label>
-      <input type="email" name="email" value={email} onChange={(e) => setEmail(e.target.value)}></input>
-      <label>password</label>
-      <input type="password" name="password" value={password} onChange={(e) => setPassword(e.target.value)}></input>
-      <button type="submit" onClick={trySignIn}>
-        SignIn
-      </button>
+      <form>
+        <label>email</label>
+        <input
+          type="email"
+          data-testid="email-input"
+          name="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <label>password</label>
+        <input
+          type="password"
+          data-testid="password-input"
+          name="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button data-testid="signin-button" type="submit" disabled={disabled} onClick={trySignIn}>
+          SignIn
+        </button>
+      </form>
     </div>
   );
 }
--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -1,10 +1,11 @@
 import { requestSignUp } from "api/api";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import swal from "sweetalert";
 
 function SignUp() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [disabled, setDisabled] = useState(true);
   const trySignUp = async () => {
     const signupResult = await requestSignUp(email, password);
     swal({
@@ -13,16 +14,38 @@ function SignUp() {
       text: signupResult.message,
     });
   };
+  useEffect(() => {
+    console.log(email.search("@"));
+    if (email.search("@") > -1 && password.length > 7) {
+      setDisabled(false);
+    } else {
+      setDisabled(true);
+    }
+  }, [email, password]);
   return (
     <div>
       <h1>SignUp</h1>
-      <label>email</label>
-      <input type="email" name="email" value={email} onChange={(e) => setEmail(e.target.value)}></input>
-      <label>password</label>
-      <input type="password" name="password" value={password} onChange={(e) => setPassword(e.target.value)}></input>
-      <button type="submit" onClick={trySignUp}>
-        SignUP
-      </button>
+      <form>
+        <label>email</label>
+        <input
+          type="email"
+          data-testid="email-input"
+          name="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <label>password</label>
+        <input
+          type="password"
+          data-testid="password-input"
+          name="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button data-testid="signup-button" type="submit" disabled={disabled} onClick={trySignUp}>
+          SignIn
+        </button>
+      </form>
     </div>
   );
 }
