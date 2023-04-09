a6198b4fe93cd80e2bc5fd299711b071faac80c7
LireEruel <jsh001505@gmail.com>
2023-04-09 14:41:18 +0900

    create todo 기능 완료

--- src/pages/ToDoList/ToDoList.jsx
+++ src/pages/ToDoList/ToDoList.jsx
@@ -1,8 +1,10 @@
-import { requestGetTodo } from "api/api";
+import { requestGetTodo, requestCreateTodo } from "api/api";
 import { useEffect, useState } from "react";
+import { swal } from "sweetalert";
 
 const ToDoList = () => {
   const [todos, setTodos] = useState([]);
+  const [inputTodo, setInputTodo] = useState("");
   useEffect(() => {
     getTodos();
   }, []);
@@ -12,9 +14,34 @@ const ToDoList = () => {
     console.log(res);
     setTodos(res.todos);
   };
+
+  const createTodo = async () => {
+    if (inputTodo.length > 0) {
+      try {
+        await requestCreateTodo(inputTodo);
+        setInputTodo("");
+        await getTodos();
+      } catch (e) {
+        swal({
+          icon: "error",
+          title: "create To do ERROR!",
+          text: "create To do",
+        });
+      }
+    }
+  };
   return (
     <>
       <h1>TODOLIST</h1>
+      <form onSubmit={createTodo}>
+        <input
+          data-testid="new-todo-input"
+          value={inputTodo}
+          onChange={(e) => setInputTodo(e.target.value)}
+          autoFocus
+        />
+        <input type="submit" data-testid="new-todo-add-button" value="Submit" />
+      </form>
       <ul>
         {todos.map((todos) => {
           return <li key={todos.id}>{todos.todo}</li>;
